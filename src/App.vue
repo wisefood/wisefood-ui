e5f1e94016ee5d3c244344c6e8b976d441637674
petroud <petroudimitris@outlook.com>
2025-10-10 07:36:22 +0000

    Removed unused old components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <v-app>
     <router-view />
-    <!-- Global off canvas system - no ref needed -->
-    <GlobalOffCanvas />
-    <!-- Global notification system - no ref needed -->
-    <NotificationAlert />
   </v-app>
 </template>
 
 <script lang="ts" setup>
-//
+
 </script>
