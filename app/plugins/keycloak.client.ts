c4e0a7fefafc51c1b8a7866d89a96f1055494b87
petroud <petroudimitris@outlook.com>
2025-12-09 13:34:18 +0000

    Keycloak bug fix

--- app/plugins/keycloak.client.ts
+++ app/plugins/keycloak.client.ts
@@ -2,6 +2,5 @@ import { defineNuxtPlugin } from '#app'
 import KeycloakAuthService from '~/services/keycloak'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const keycloakService = KeycloakAuthService
-  nuxtApp.provide('keycloak', keycloakService)
+  nuxtApp.provide('keycloak', KeycloakAuthService)
 })
